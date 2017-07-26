40f1162ab6f24790d321f3e09ea3e192674bf2a3
xyn <545519799@qq.com>
2017-07-26 16:52:37 +0800

    日常更新-2017年7月26日16:52:35

--- demo/cookie/server.php
+++ demo/cookie/server.php
@@ -1,6 +1,8 @@
 <?php
 header('Access-Control-Allow-Origin:*');
+session_start();
 $server=$_GET;
+$_SESSION["get"]=json_encode($server);
 $server["response"]=array(
     "msg"=>"服务器返回数据",
     "time"=>time()
