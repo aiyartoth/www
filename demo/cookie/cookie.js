a93cdc3410b5d8d0339ceb0a376ff3053513297b
xyn <545519799@qq.com>
2017-07-26 17:12:09 +0800

    cookie方法增加

---
+++ demo/cookie/cookie.js
@@ -0,0 +1,24 @@
+//参考资料 http://www.cnblogs.com/swing07/p/5715824.html
+var cookie_method = {
+    get: function (cname) {
+        //todo 获取cookie
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') c = c.substring(1);
+            if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+        }
+        return "";
+    },
+    set: function (cname, cvalue, exdays) {
+        //todo 设置cookie
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        var expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + "; " + expires;
+    },
+    clear: function (cname) {
+        this.setCookie(name, "", -1);
+    }
+};
